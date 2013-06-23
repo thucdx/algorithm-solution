61bd1db43c697a38df3d7f4a13aa82139b14d48a
timxad <thucdx31@gmail.com>
2013-06-23 16:06:36 +0700

    + Add MIXTURES.cpp

---
+++ FOJ/Practice/MIXTURES.cpp
@@ -0,0 +1,23 @@
+#include <set>
+#include <map>
+#include <cmath>
+#include <vector>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+#define FOR(i, a, b) for (int i = (a), _b = (b); i <= _b; ++i)
+#define DOW(i, A, a) for (int i = (A), _a = (a); i >= _a	; --i)
+#define REP(i, n) for (int i = 0, _n = (n); i < _n; ++i)
+
+int main() {
+#ifdef LOCAL
+	freopen("./IO/input", "r", stdin);
+	freopen("./IO/output", "w", stdout);
+#endif
+
+}
