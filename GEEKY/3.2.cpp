3d58bad9908bd350da12f21172fe58bf2938bd28
thucdx31 <thucdx31@gmail.com>
2013-09-26 12:56:51 +0700

    Update 3.2.cpp

--- GEEKY/3.2.cpp
+++ GEEKY/3.2.cpp
@@ -18,50 +18,46 @@ using namespace std;
 #define DOW(i, A, a) for (int i = (A), _a = (a); i >= _a; --i)
 #define REP(i, n) for (int i = 0, _n = (n); i < _n; ++i)
 #define FIT(it, v) for (typeof((v).begin()) it = (v).begin(); it != (v).end(); ++it)
-
 #define dbg(x) cout << #x << " " << x << endl
 
 typedef long long ll;
 
-#define MESSAGE_LENGTH 199
-
-string ACCEPT_SYMBOL = " .,\'";
 string result = "there's no reality except the one contained within us. that's why so many people live an unreal life. they take images outside them for reality and never allow the world within them to assert itself.";
-const ll MAX_CHAR = (ll) 1e10;
-
+string ACCEPT_SYMBOL = " .,\'";
 bool isOk[260];
 void init() {
-	FOR(c, 'a', 'z') isOk[c] = true;
-	FOR(c, 'A', 'Z') isOk[c] = true;
-	FOR(c, '0', '9') isOk[c] = true;
-	REP(i, ACCEPT_SYMBOL.length()) isOk[ACCEPT_SYMBOL[i]] = true;
+	FOR (c, 'a', 'z') isOk[c] = true;
+	FOR (c, 'A', 'Z') isOk[c] = true;
+	FOR (c, '0', '9') isOk[c] = true;
+	REP (i, ACCEPT_SYMBOL.length()) isOk[ACCEPT_SYMBOL[i]] = true;
 }
+
+#define MESSAGE_LENGTH 199
 
 int main() {
 #ifdef LOCAL
 	freopen("D:/GK/datdata/data.dat", "r", stdin);
 //	freopen("./IO/output", "w", stdout);
 #endif
-
 	init();
 
 	ll pos = 0;
 	int totalOk = 0;
 	deque<char> q;
-	char curChar, front;
+	char curChar;
 
-	while (scanf("%c", &curChar) != EOF && pos < MAX_CHAR) {
-		if (isOk[curChar]) totalOk++;
+	while (scanf("%c", &curChar) != EOF) {
+		if (isOk[curChar]) ++totalOk;
 		q.push_back(curChar);
 		if (pos >= MESSAGE_LENGTH) {
-			front = q.front(); q.pop_front();
-			if (isOk[front]) totalOk--;
+			if (isOk[q.front()]) --totalOk;
+			q.pop_front();
 		}
 
 		if (totalOk == MESSAGE_LENGTH) {
 			bool isOk = true;
 			int i = 0;
-			FIT(it, q) {
+			FIT (it, q) {
 				if (*it != result[i]) { isOk = false; break; }
 				++i;
 			}
